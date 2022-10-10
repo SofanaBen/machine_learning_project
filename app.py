036604e726aa2ae43cc92df6a9f3f3859a0c64ba
sofana benoutiq <sofanabenoutiq@sofanas-MacBook-Pro-2.local>
2022-10-10 13:42:16 -0400

    Added exception

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from crypt import methods
 from distutils.log import debug
+import sys
 from flask import Flask
-
+from housing.logger import logging
+from housing.exception import HousingException
 app=Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 def index():
+    try:
+        raise Exception("We are raising exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
     return "Starting Machine Learning Project"
 
 if __name__=="__main__":
